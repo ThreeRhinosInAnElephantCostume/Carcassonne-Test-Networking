1fcb96d29e2545bebcb6861528798705f664128e
developer <you@example.com>
2021-11-25 00:42:03 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Carcassonne_Test_Networking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
